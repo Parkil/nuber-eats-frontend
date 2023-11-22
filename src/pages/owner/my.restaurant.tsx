a4aa935b0582589c6d610d1b8a5632f8a2c87b1f
Parkil <alkain77@gmail.com>
2023-11-23 08:28:43 +0900

    owner dashboard 구현

--- src/pages/owner/my.restaurant.tsx
+++ src/pages/owner/my.restaurant.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import {Link, useParams} from "react-router-dom";
 import {gql, useQuery} from "@apollo/client";
-import {DISH_FRAGMENT, RESTAURANT_FRAGMENT} from "../../constant/fragments";
+import {DISH_FRAGMENT, ORDER_FRAGMENT, RESTAURANT_FRAGMENT} from "../../constant/fragments";
 import {OwnerRestaurantQuery, OwnerRestaurantQueryVariables} from "../../__graphql_type/type";
+import {Dish} from "../../components/dish/dish";
+import {
+  VictoryAxis,
+  VictoryChart,
+  VictoryLabel,
+  VictoryLine,
+  VictoryTheme,
+  VictoryTooltip,
+  VictoryVoronoiContainer
+} from "victory";
 
 interface IRestaurantParams {
   id: string;
@@ -18,11 +28,15 @@ export const OWNER_RESTAURANT_QUERY = gql`
         menu {
           ...DishParts
         }
+        orders {
+          ...OrderParts
+        }
       }
     }
   } 
   ${RESTAURANT_FRAGMENT}
   ${DISH_FRAGMENT}
+  ${ORDER_FRAGMENT}
 `;
 
 export const MyRestaurant = () => {
@@ -52,7 +66,34 @@ export const MyRestaurant = () => {
             </Link>
           </div>
           <div className="mt-10">
-            {data?.ownerRestaurant.restaurant?.menu.length === 0 ? <h4 className="text-xl mb-5">Please a Update Dish</h4> : <h4>요리111</h4>}
+            {data?.ownerRestaurant.restaurant?.menu.length === 0 ?
+              <h4 className="text-xl mb-5">Please a Update Dish</h4> :
+              <div className="grid md:grid-cols-3 gap-x-5 gap-y-10">
+                {data?.ownerRestaurant.restaurant?.menu.map((dish) =>
+                  <Dish key={dish.name} description={dish.description} name={dish.name} price={dish.price}/>)}
+              </div>
+            }
+          </div>
+          <div className="mt-20 mb-10">
+            <h4 className="text-center text-2xl font-medium">Sales</h4>
+            <div>
+              <VictoryChart width={window.innerWidth} height={500} containerComponent={<VictoryVoronoiContainer/>}
+                            domainPadding={50} theme={VictoryTheme.material}>
+                <VictoryLine
+                  data={
+                    data?.ownerRestaurant.restaurant?.orders.map(order => ({x: order.createdAt, y: order.total}))
+                  }
+                  labels={({datum}) => `$${datum.y}`}
+                  labelComponent={<VictoryTooltip renderInPortal dy={-20}/>}
+                  style={{data: {strokeWidth: 3}}}
+                  interpolation={'natural'}
+                />
+                {/*<VictoryAxis dependentAxis style={{tickLabels: {fontSize:17, fill: 'red'} }} tickFormat={tick => `$${tick}`}/>*/}
+                <VictoryAxis tickFormat={tick => new Date(tick).toLocaleDateString('ko')}
+                             tickLabelComponent={<VictoryLabel renderInPortal/>}
+                             style={{tickLabels: {fontSize: 15, fill: 'red', angle: 45}}}/>
+              </VictoryChart>
+            </div>
           </div>
         </div>
       }
