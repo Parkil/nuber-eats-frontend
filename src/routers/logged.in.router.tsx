25fe0b75936850605b83efd9a323496387129c24
Parkil <alkain77@gmail.com>
2023-11-26 10:54:21 +0900

    레스토랑 카테고리별 리스트 삭제

--- src/routers/logged.in.router.tsx
+++ src/routers/logged.in.router.tsx
@@ -5,7 +5,6 @@ import {Header} from "../components/header";
 import {useMe} from "../hooks/use.me";
 import {ConfirmEmail} from "../pages/user/confirm.email";
 import {EditProfile} from "../pages/user/edit.profile";
-import {Category} from "../pages/client/category";
 import {Restaurant} from "../pages/client/restaurant";
 import {SetRoute} from "../components/set.route";
 import {MyRestaurants} from "../pages/owner/my.restaurants";
